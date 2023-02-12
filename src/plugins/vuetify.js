924b77852861f21ad20c5b4a0ebe555deac98231
FabilousK <f.kaltenbach@webulous-echo.com>
2023-02-12 22:16:31 +0100

    apptheme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,7 +8,7 @@ import { createVuetify } from 'vuetify';
 
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'light2',
     themes: {
       light: {
         dark: false,
@@ -20,6 +20,22 @@ export default createVuetify({
           'primary-lighten-4': '#b6e6b3',
           'primary-lighten-5': '#daf2d9',
           background: '#ecf8ec',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+          error: '#B00020',
+        },
+      },
+      light2: {
+        dark: false,
+        colors: {
+          primary: '#113B49', // #009091
+          'primary-lighten-1': '#185467',
+          'primary-lighten-2': '#227591',
+          'primary-lighten-3': '#2786a5',
+          'primary-lighten-4': '#30a7cf',
+          'primary-lighten-5': '#eaf6fa',
+          background: '#eaf6fa',
           info: '#2196F3',
           success: '#4CAF50',
           warning: '#FB8C00',
